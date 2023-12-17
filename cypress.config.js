d8960f23bc3ea78d20203092e94c3c955d3f001f
RaulSilveiraa <raul.silveira@melhorenvio.com>
2023-12-17 14:33:53 -0300

    adição do Cypress cloud

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,12 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  projectId: "adhaau",
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
+      projectId: "adhaau"
     },
   },
 });
+
